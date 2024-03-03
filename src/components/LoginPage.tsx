21cc2dc7b5ffb45dfd407a1405310f8583aa8c9b
Renier Vargas <renier1989@gmail.com>
2024-03-03 12:30:56 -0500

    implementando Zustand, para simular un login y un logout , ejecutanco metodos

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,15 +1,52 @@
+import { useEffect } from "react"
 import { useAuthStore } from "../store/auth.store"
 
 export const LoginPage = () => {
     // asi es como se consumen los state en un store de Zustand
     const authState = useAuthStore(state => state.status)
+    const user = useAuthStore(state => state.user)
+    const login = useAuthStore(state => state.login)
+    const logout = useAuthStore(state => state.logout)
 
-  return (
-    <>
-    <h1 className="text-3xl font-semibold">Login Page</h1>
-    <span>
-        {authState}
-    </span>
-    </>
-  )
+    useEffect(() => {
+        setTimeout(() => {
+            logout()
+        }, 1500);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+    if (authState === 'checking') {
+        return <>
+            <h1 className="text-3xl font-semibold">Login Page</h1>
+            <h1 className="text-xl font-semibold"> Cargando...</h1>
+        </>
+    }
+
+    return (
+        <>
+            <h1 className="text-3xl font-semibold">Login Page</h1>
+            {
+                (authState === 'authenticated')
+                    ?
+                    <> <span>Autenticado como: {JSON.stringify(user, null, 2)} </span> </>
+                    :
+                    <> <span className="text-rose-500 font-semibold text-lg">No Autenticado </span> </>
+            }
+            <br />
+            {
+                (authState === 'authenticated') 
+                ?
+                <>
+                <button onClick={logout} className="p-2 rounded bg-rose-400 text-white mr-2"> Cerrar Sesion </button>
+                </>
+                :
+                <>
+                <button onClick={()=>login('Renier@gmail.com', '123')}
+                className="p-2 rounded bg-green-400 text-white mr-2"
+                > Iniciar Sesion </button>
+                </>
+            }
+        </>
+    )
 }
--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -6,12 +6,36 @@ interface AuthState {
     user?: {
         name: string;
         email: string;
-    }
+    },
+
+    // definicion de metods
+    login : (email: string , password: string) => void;
+    logout : () => void;
+
 }
 
 // esto es la definicion global de un store en Zustand , se podra usar en toda la aplicacion
 export const useAuthStore = create<AuthState>()( ( set )=>({
     status: 'checking',
     token: undefined,
-    user: undefined
+    user: undefined,
+    login : (email: string , password: string) => {
+        // esto simula una llamada a un Backend, y seteamos con el set el valor obtenido
+        set({
+            status: 'authenticated',
+            token: 'XCV123',
+            user: {
+                name: 'Renier Vargas',
+                email: email
+            }
+        })
+    },
+    logout : ()=> {
+        // el logout limpiar los parametros
+        set({
+            status: 'unauthenticated',
+            token: undefined,
+            user: undefined
+        })
+    }
 }))
