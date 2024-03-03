a5cea0eda48a53a79d517749df78bff464b450cb
Renier Vargas <renier1989@gmail.com>
2024-03-03 13:17:31 -0500

    Mostrando los usaurios en una tabla y mejorando el rendimiento, seprando en componentes y definiendo el tipado de lo que reciben los componentes

--- src/components/UserPages.tsx
+++ src/components/UserPages.tsx
@@ -1,20 +1,25 @@
 import axios from "axios"
-import { useEffect } from "react"
-import { ReqResUserListResponse } from "../interfaces"
+import { useEffect, useState } from 'react';
+import type { ReqResUserListResponse, User } from "../interfaces"
 
-const loadUser = async () => {
+const loadUser = async (): Promise<User[]> => {
     try {
         const { data } = await axios<ReqResUserListResponse>('https://reqres.in/api/users');
         return data.data
     } catch (error) {
         console.log(error);
-        return;
+        return [];
     }
 }
 
 export const UserPages = () => {
+    const [users, setUsers] = useState<User[]>([])
     useEffect(() => {
-        loadUser().then(users => console.log(users));
+        // loadUser().then(users => console.log(users));
+        // loadUser().then(users => setUser(users)); // una version comun para setear en el state los usuarios de la peticion
+
+        // una version mas moderna de sertear usuario al state.  esto se hace porque solo se le pasa un solo aprametro que son los users de la peticion y como solo retorna eso se le asigna autematicamente al setUser haciendo la referencia del metodo
+        loadUser().then(setUsers);
     }, [])
 
 
@@ -27,27 +32,35 @@ export const UserPages = () => {
                     <tr>
                         <th>Avatar</th>
                         <th>Nombre</th>
-                        <th>Edad</th>
+                        <th>Email</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>avatar</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
-                    </tr>
-                    <tr>
-                        <td>avatar 1</td>
-                        <td>The Eagles</td>
-                        <td>1972</td>
-                    </tr>
-                    <tr>
-                        <td>avatar 2</td>
-                        <td>Earth, Wind, and Fire</td>
-                        <td>1975</td>
-                    </tr>
+                    {
+                        users.map(user => (
+                            <UserRow key={user.id} user={user} />
+                        ))
+                    }
                 </tbody>
             </table>
         </>
     )
+}
+
+
+// aqui se define un componente que contiene los rows de los usuarios
+// esto se hacer para mejorar el rendimiento y asignar un componente por cada resutlado. y poder manipular o hacer ejecucion solo sobre ese componente y evitar re-renders.
+// tambie se debe definir un interface que me diga cuales son las propos que recibe este componente
+interface Props {
+    user: User
+}
+export const UserRow = ({ user }: Props) => {
+    const { avatar, first_name, last_name, email } = user
+    return (
+        <tr className=" rounded shadow">
+            <td> <img className="w-20 rounded-lg" src={avatar} alt={first_name} /> </td>
+            <td> <span className="ml-4 font-semibold text-xl text-gray-500"> {first_name} {last_name}</span></td>
+            <td className="text-lg text-gray-400 font-semibold ">{email}</td>
+        </tr>
+    );
 }
